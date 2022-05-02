3fee0c591db4035f0d1b862033e5f0753e0b75a9
David Winkler <100160061+DavidWinkler11@users.noreply.github.com>
2022-05-03 01:52:32 +0200

    Add files via upload
    Sinkronizacija dretvi i procesa pomoću radnog čekanja

--- signali.c
+++ signali.c
@@ -1,97 +1,177 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <signal.h>
-#include <math.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <pthread.h>
 
-/* funkcije za obradu signala, navedene ispod main-a */
-void obradi_SIGUSR1(int sig);
-void obradi_SIGTERM(int sig);
+int Id; /* identifikacijski broj segmenta */
 
 int broj;
-FILE *status;
-FILE *obrada;
+int *ZajednickaVarijabla = 0; // za procese
+int RadnaIzlazna = 0;         // za dretve
 
-int nije_kraj = 1;
-int i;
+FILE *file;
+int b;
 
-int main()
+void brisi(int sig)
 {
-    struct sigaction act;
-
-    /* 1. maskiranje signala SIGUSR1 */
-    act.sa_handler = obradi_SIGUSR1; /* kojom se funkcijom signal obrađuje */
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;               /* naprednije mogućnosti preskočene */
-    sigaction(SIGUSR1, &act, NULL); /* maskiranje signala preko sučelja OS-a */
-
-    /* 2. maskiranje signala SIGTERM */
-    act.sa_handler = obradi_SIGTERM;
-        sigemptyset(&act.sa_mask);
-    sigaction(SIGTERM, &act, NULL);
-
-    printf("Program s PID=%ld krenuo s radom\n", (long)getpid());
-
-    status = fopen("status.txt", "r");
-    fscanf(status, "%d", &broj);
-    fclose(status);
-
-    printf("Broj zapisan u status.txt je: %d\n", broj); // PROVJERA
-
-    if (broj == 0)
-    {
-        obrada = fopen("obrada.txt", "r");
-        while ((getc(obrada)) != EOF)
-        {
-            fscanf(obrada, "%d", &broj);
-        }
-        broj = sqrt(broj);
-        fclose(obrada);
-    }
-
-    printf("Prvi broj koji slijedi u obradi %d\n", broj + 1);
-    int nula = 0; // POSTAVLJANJE STATUSA NA NULU
-    status = fopen("status.txt", "w");
-    fprintf(status, "%d", nula);
-    fclose(status);
-
-    /* neki posao koji program radi; ovdje samo simulacija */
-
-    while (nije_kraj)
-    {
-        obrada = fopen("obrada.txt", "a");
-        fprintf(obrada, "%d\n", (broj + 1) * (broj + 1));
-        fclose(obrada);
-
-        printf("%d\n", (broj + 1) * (broj + 1)); // PROVJERA
-        broj++;
-
-        for (int j = 0; j < 5; j++)
-        {
-            sleep(1);
-        }
-    }
-    printf("Program s PID=%ld zavrsio s radom\n", (long)getpid());
-    return 0;
+   printf("Brisem! \n");
+   /* oslobađanje zajedničke memorije */
+   (void)shmdt((char *)ZajednickaVarijabla);
+   (void)shmctl(Id, IPC_RMID, NULL);
+   exit(0);
 }
 
-void obradi_SIGUSR1(int sig)
+void writeToFile(int x)
 {
-    printf("BROJ U OBRADI JE: %d\n", broj); // PROVJERA
+   file = fopen("file.txt", "a");
+   fprintf(file, "%d\n", x);
+   fclose(file);
 }
 
-void obradi_SIGTERM(int sig)
+void *ulazna()
 {
-    status = fopen("status.txt", "w");
-    fprintf(status, "%d", broj);
-    fclose(status);
 
-    status = fopen("status.txt", "r");
-    fscanf(obrada, "%d", &broj);
-    fclose(status);
+   for (int k = 0; k < broj; k++)
+   { // određeni broj random brojeva
 
-    // printf("BROJ NA KRAJU JE: %d\n", broj); PROVJERA
-    printf("Primio signal SIGTERM, spremam broj u obradi (%d) u status.txt!\n", broj);
-    nije_kraj = 0;
+      do
+      { // RADNO ČEKANJE
+         sleep(1);
+      } while (*ZajednickaVarijabla != 0);
+
+      for (int j = 0; j < rand() % 5 + 1; j++)
+      { // "Svakih 1-5 sekundi
+         sleep(1);
+      }
+
+      *ZajednickaVarijabla = rand() % 100 + 1;
+      printf("\nULAZNA DRETVA: generiran je broj %d\n", *ZajednickaVarijabla);
+   }
 }
 
+void *radna()
+{
+   for (int k = 0; k < broj; k++)
+   {
+
+      do
+      { // RADNO ČEKANJE
+         sleep(1);
+         // printf("Zapeo sam! \n");
+
+      } while (*ZajednickaVarijabla == 0);
+
+      RadnaIzlazna = *ZajednickaVarijabla + 1;
+      printf("RADNA DRETVA: pročitan broj %d i povećan na %d\n", *ZajednickaVarijabla, RadnaIzlazna);
+
+      *ZajednickaVarijabla = 0;
+   }
+}
+
+void *izlazna()
+{
+
+   for (int k = 0; k < broj; k++)
+   {
+
+      do
+      {
+
+         sleep(1);
+      } while (RadnaIzlazna == 0);
+
+      printf("IZLAZNI PROCES: broj upisan u datoteku %d\n\n", RadnaIzlazna);
+      writeToFile(RadnaIzlazna);
+      RadnaIzlazna = 0;
+   }
+}
+
+int main(void)
+{
+   int x;
+   srand(time(NULL));
+
+   printf("Upisite broj: ");
+   scanf("%d", &x);
+   printf("\n");
+
+   broj = x;
+
+   // ZAUZIMANJE ZAJEDNIČKE MEMORIJE
+   Id = shmget(IPC_PRIVATE, sizeof(int), 0600);
+
+   if (Id == -1)
+   {
+      printf("Nema zajedničke memorije");
+      exit(1); // NEMA ZAJEDNIČKE MEMORIJE - GREŠKA
+   }
+
+   ZajednickaVarijabla = (int *)shmat(Id, NULL, 0);
+   *ZajednickaVarijabla = 0;
+
+   struct sigaction act;
+   sigaction(SIGINT, &act, NULL); // U SLUČAJU PREKIDA BRIŠI MEMORIJU
+
+   pthread_t thr_id, thread_id[2];
+
+   // POKRETANJE PARALELNIH PROCESA
+   if (fork() == 0)
+   {
+
+      if (pthread_create(&thr_id, NULL, ulazna, NULL) != 0)
+      {
+         printf("Greska pri stvaranju dretve!\n");
+         exit(1);
+      }
+      else
+      {
+         printf("Pokrenuta ULAZNA DRETVA \n");
+      }
+
+      pthread_join(thr_id, NULL);
+      exit(0);
+   }
+
+   if (fork() == 0)
+   {
+      if (pthread_create(&thread_id[0], NULL, radna, NULL) != 0)
+      {
+         printf("Greska pri stvaranju dretve!\n");
+         exit(1);
+      }
+      else
+      {
+         printf("Pokrenuta RADNA DRETVA\n");
+      }
+
+      if (pthread_create(&thread_id[1], NULL, izlazna, NULL) != 0)
+      {
+         printf("Greska pri stvaranju dretve!\n");
+         exit(1);
+      }
+      else
+      {
+         printf("Pokrenuta IZLAZNA DRETVA\n");
+      }
+
+      pthread_join(thread_id[0], NULL);
+      pthread_join(thread_id[1], NULL);
+
+      exit(0);
+   }
+
+   (void)wait(NULL);
+   (void)wait(NULL);
+   printf("\nZavršila ULAZNA DRETVA\n");
+   printf("Završila RADNA DRETVA\n");
+   printf("Završio IZLAZNI PROCES\n");
+   brisi(0);
+
+   return 0;
+}
